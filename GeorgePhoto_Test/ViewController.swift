787cb44a5dbd6b59b6b0d6539fac18916580168f
George <george.groshev1997@gmail.com>
2022-11-29 12:24:07 +0200

    Initial Commit

---
+++ GeorgePhoto_Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GeorgePhoto_Test
+//
+//  Created by Georgiy Groshev on 29.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
